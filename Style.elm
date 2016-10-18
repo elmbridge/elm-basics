886b5a2da43db74b95e61a4b3e9e1393749970fa
Aaron VonderHaar <gruen0aermel@gmail.com>
2016-10-17 21:39:29 -0700

    Split Main.elm into separate files

---
+++ Style.elm
@@ -0,0 +1,24 @@
+module Style exposing (..)
+
+import Color
+
+
+--
+-- Customize your emoji and colors
+--
+
+
+successEmoji =
+    "💖"
+
+
+successColor =
+    Color.green
+
+
+keepWorkingEmoji =
+    "🚧"
+
+
+keepWorkingColor =
+    Color.yellow
